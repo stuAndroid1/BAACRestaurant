4cee0410b77af0ed2b1b2cb6640b6b1ee906ab5c
stuAndroid <oa2baac@gmail.com>
2015-10-19 16:06:05 +0700

    Finish Day1

---
+++ app/src/main/java/com/example/baac/baacrestaurant/FoodTABLE.java
@@ -0,0 +1,38 @@
+package com.example.baac.baacrestaurant;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by BAAC on 19-Oct-15.
+ */
+public class FoodTABLE {
+
+    //Explicit
+    private MyOpenHelper objMyOpenHelper;
+    private SQLiteDatabase writeSqLiteDatabase, readSqLiteDatabase;
+
+    public static  final String FOOD_TABLE = "foodTABLE";
+    public static final String COLUMN_ID_FOOD = "_id";
+    public static final String COLUMN_FOOD = "Food";
+    public static final String COLUMN_SOURCE = "Source";
+    public static final String COLUMN_PRICE = "Price";
+
+
+    public FoodTABLE(Context context) {
+        objMyOpenHelper = new MyOpenHelper(context);
+        writeSqLiteDatabase = objMyOpenHelper.getWritableDatabase();
+        readSqLiteDatabase = objMyOpenHelper.getReadableDatabase();
+
+    }   // Constructor
+
+    public long addNewFood(String strFood, String strSource, String strPrice) {
+        ContentValues objContentValues = new ContentValues();
+        objContentValues.put(COLUMN_FOOD, strFood);
+        objContentValues.put(COLUMN_SOURCE, strSource);
+        objContentValues.put(COLUMN_PRICE, strPrice);
+        return writeSqLiteDatabase.insert(FOOD_TABLE,null,objContentValues);
+    }
+
+}   // Main Class
--- app/src/main/java/com/example/baac/baacrestaurant/MainActivity.java
+++ app/src/main/java/com/example/baac/baacrestaurant/MainActivity.java
@@ -7,6 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
     //Explicit
     private UserTABLE objUserTABLE;
+    private FoodTABLE objFoodTABLE;
 
 
 
@@ -18,11 +19,20 @@ public class MainActivity extends AppCompatActivity {
         //Create & Connected Database
         createAndConnected();
 
+        //Tester Add New Value
+        testerAdd();
+
     }   //Main Method
+
+    private void testerAdd() {
+        objUserTABLE.addNewUser("testUser", "testPassword", "ทดสอบขื่อภาษาไทย");
+        objFoodTABLE.addNewFood("ชื่ออาหาร","testSource","123");
+    }
 
     private void createAndConnected() {
 
         objUserTABLE = new UserTABLE(this);
+        objFoodTABLE = new FoodTABLE(this);
 
     }
 }   // Main Class
